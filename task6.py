50713fd9553bda0da6bcae3a00cf6db692d8c129
Sean Mohomane <karabo.mn33@gmail.com>
2020-11-29 17:28:56 +0200

    bug has been fixed

--- task6.py
+++ task6.py
@@ -1,15 +1,16 @@
 def max_num(num_1, num_2, num_3):
     contain = num_1, num_2, num_3
-    num = num_1
+    num = 0
 
     for i in contain:
-        if i > num:
-            num = i
-        elif i < -num:
-            num = i
-        elif i == 0:
-            num = i
+        if i > 0:
+            if i > num:
+                num = i
+            elif i < -num:
+                num = i
+        else:
+            num == 0
     return num
 
 
-print(max_num(-150, 0, -100))
+print(max_num(-150, 0, -130))
