47d35eb95e780e608fc09f8b90766301d453f8b5
Sean Mohomane <karabo.mn33@gmail.com>
2020-12-04 20:09:31 +0200

    update task 8

--- task8.py
+++ task8.py
@@ -2,7 +2,7 @@ import math
 
 
 def time(num):
-    hours = math.floor(num/60)
+    hours = int(math.floor(num/60))
     minutes = num % 60
 
     if hours > 1 and minutes > 1:
@@ -13,6 +13,10 @@ def time(num):
         print(str(hours) + " hours, " + str(minutes) + " minute")
     elif hours == 1 and minutes > 1:
         print(str(hours) + " hour, " + str(minutes) + " minutes")
+    elif minutes > 1:
+        print(str(minutes), 'minutes')
+    elif minutes == 1:
+        print(str(minutes), 'minute')
 
 
-time(71)
+time(1)
